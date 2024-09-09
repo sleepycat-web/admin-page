f7910afd00af286880d48183f476b6b6d9ac9e34
Amlan Sarmah <amlansarmah26@gmail.com>
2024-09-09 17:54:03 +0530

    add database

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -8,26 +8,17 @@ if (!uri) {
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-declare global {
-  var _mongoClientPromise: Promise<MongoClient>;
-}
+
 
 export async function connectToDatabase() {
-  if (process.env.NODE_ENV === "development") {
-    // In development mode, use a global variable to preserve the client across module reloads
-    if (!global._mongoClientPromise) {
-      client = new MongoClient(uri);
-      global._mongoClientPromise = client.connect();
-    }
-    clientPromise = global._mongoClientPromise;
-  } else {
+ 
     // In production mode, it's best to not use a global variable
     client = new MongoClient(uri);
     clientPromise = client.connect();
-  }
+  
 
   return {
-    db: (await clientPromise).db(),
+    db: (await clientPromise).db("ChaiMine"),
     client,
   };
 }
