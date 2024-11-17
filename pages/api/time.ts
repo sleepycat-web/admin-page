4c1bf6a0727ef39db1356a4c6f5674b4c713d4b8
Amlan Sarmah <amlansarmah26@gmail.com>
2024-11-18 01:01:02 +0530

    remove time middleware and api

--- pages/api/time.ts
+++
@@ -1,6 +0,0 @@
-export async function GET() {  // for App Router
-  return Response.json({ 
-    time: new Date().toString(),
-    timeIST: new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })
-  })
-}
